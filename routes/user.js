cb1112b817c0ea32d459cdb9c937491f420c18db
bilalahmed804 <ba3801534@gmail.com>
2025-01-25 16:56:23 +0500

    backend_update

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import bcrypt from 'bcryptjs';
 import user from "../models/user.js";
+import jwt from "jsonwebtoken"
 
 const router = Router();
 
 // SignUp API
-router.post('/signup', async (req, res) => {
+router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
 
   try {
@@ -20,19 +21,22 @@ router.post('/signup', async (req, res) => {
 });
 
 // SignIn API
-router.post('/signin', async (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
+console.log("user",email,password);
 
-  try {
-    const foundUser = await user.findOne({ email });
-    if (!foundUser) return res.status(400).json({ message: 'User not found!' });
 
-    const isPasswordValid = await bcrypt.compare(password, foundUser.password);
-    if (!isPasswordValid) {
-      return res.status(400).json({ message: 'Invalid credentials!' });
-    }
-
-    res.status(200).json({ message: 'SignIn successful' });
+try {
+  const foundUser = await user.findOne({ email });
+  if (!foundUser) return res.status(400).json({ message: 'User not found!' });
+  
+  const isPasswordValid = await bcrypt.compare(password, foundUser.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({ message: 'Invalid credentials!' });
+  }
+  
+  const token = jwt.sign({user}, jwtSecret , { expiresIn: '24h' });
+  res.status(200).json({ message: 'SignIn successful' });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
